649277d183ca71e50b38fa5ed3d9af5e4b36ff65
Dmitriy Kudlovich <carnagery@mail.ru>
2023-01-01 17:12:52 +0300

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("hello");
+
+function putStartCardPlan() {
+  let productCardcurrent = document.querySelectorAll(
+    ".product-card__list-current"
+  );
+  let productCardLi = document.querySelectorAll(".product-card__li");
+}
